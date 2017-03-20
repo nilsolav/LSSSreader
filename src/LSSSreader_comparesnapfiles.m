3ef1ceef0c5f4987176670caa1f52017048d6e4a
Nils Olav Handegard <nils.olav.handegard@imr.no>
2017-03-20 16:30:17 +0100

    first version on the LSSSreader_comparesnapfiles. Not yet a working version. first cut.

--- src/LSSSreader_comparesnapfiles.m
+++ src/LSSSreader_comparesnapfiles.m
@@ -1,23 +1,26 @@
 function status=LSSSreader_comparesnapfiles(file1,file2)
 %
-% this function takes the output from LSSSreader_readsnapfiles and compares
-% the content of the two files.
+% this function takes the output structure from LSSSreader_readsnapfiles
+% and compares the content of the two files.
 %
 % inout :
-% file1 : File to be compared
-% file2 : File to be compared
+% file1 : Output from LSSSreader_readsnapfiles
+% file2 : Output from LSSSreader_readsnapfiles
 %
 % Output
-% status.equal : False if the content is different
-%
+% status : Status of the comparison
+% status = 1 : No changes detected
+% status = 2 : Different numbers of regions
+% status = 3 : Same number of regions but different area
 
-status =1;
+status = 1;
 % Loop over structures
 if length(file1)~=length(file2)
     msg = 'Different number of polygons';
     status = 2;
 else
-%     for i=1:length(file1)
-%         file1{i}
-%     end
+    for i=1:length(file1)
+        file1{i}
+    end
 end
+
