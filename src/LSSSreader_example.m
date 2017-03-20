544baecf7d67d78b4b083cab66424b1f1f58ad29
Nils Olav Handegard <nils.olav.handegard@imr.no>
2017-03-20 16:01:36 +0100

    Change the example to work with rdir so it can be used on calisto data.

--- src/LSSSreader_example.m
+++ src/LSSSreader_example.m
@@ -1,108 +1,87 @@
-%% clean slate for some of us
-user = char(java.lang.System.getProperty('user.name'));
-if ~strcmp(user, 'gavinj')
-    clear
-    close all
-    clc
-else
-    warning('I must ensure that I do not amuse my self too much. Not healthy.')
-end
+%% Initialize
 
 whr = which('LSSSreader_readsnapfiles');
 [dr,~,~] = fileparts(whr);
+dr = dr(1:end-3);
 
-directory(1).snap_dir = fullfile(dr,'/../exampledata/S2006101/');
-directory(2).snap_dir = fullfile(dr,'/../exampledata/S2016837/');
-directory(3).snap_dir = fullfile(dr,'/../exampledata/S2005114/');
+% Recursively list relevant data in the example directory. This can be used
+% to search files in callisto.
+files.snap = rdir(fullfile(dr,'exampledata','**','*.snap'));
+files.work = rdir(fullfile(dr,'exampledata','**','*.work'));
+files.raw = rdir(fullfile(dr,'exampledata','**','*.raw'));
 
-d=LSSSreader_pairfiles(directory);
+% Match the different files
+files=LSSSreader_pairfiles(files);
 
-
-%% Get the paths to the test data
-
-
-par(1).raw_file  = fullfile(dr,'/../exampledata/S2006101/tokt2006101-D20060124-T030844.raw');
-par(1).snap_file = fullfile(dr,'/../exampledata/S2006101/tokt2006101-D20060124-T030844.work');
-
-par(2).raw_file  = fullfile(dr,'/../exampledata/S2016837/2016837-D20160503-T093515.raw');
-par(2).snap_file = fullfile(dr,'/../exampledata/S2016837/2016837-D20160503-T093515.work');
-
-par(3).raw_file  = fullfile(dr,'/../exampledata/S2005114/tokt2005114-D20051118-T062010.raw');
-par(3).snap_file  = fullfile(dr,'/../exampledata/S2005114/tokt2005114-D20051118-T062010.snap');
-
-% Example with two types of erased region and an exclude region
-par(4).raw_file  = fullfile(dr,'/../exampledata/S2014119/2014119-D20141029-T172311.raw');
-par(4).snap_file  = fullfile(dr,'/../exampledata/S2014119/2014119-D20141029-T172311.work');
-
-
-for i=1:length(par)
-    if ~exist(par(i).raw_file)|~exist(par(i).snap_file)
-        disp('Files are not present')
-    end
-end
 
 %% Pick a file
-for file=1:4
-snap = par(file).snap_file;
-raw  = par(file).raw_file;
-
-%% Read snap file
-[school,layer,exclude,erased] = LSSSreader_readsnapfiles(snap);
-
-%% Read raw file and convert to sv
-[raw_header,raw_data] = readEKRaw(raw);
-raw_cal = readEKRaw_GetCalParms(raw_header, raw_data);
-Sv = readEKRaw_Power2Sv(raw_data,raw_cal);
-
-% Get the transducer depth
-f=1;
-if length(raw_data.pings) > 1
-    f=2; % Use 38 kHz if we can (which is usually channel 2 on IMR ships)
-end
-td = double(median(raw_data.pings(f).transducerdepth));
-
-%% Plot result
-[fh, ih] = readEKRaw_SimpleEchogram(Sv.pings(f).Sv, 1:length(Sv.pings(f).time), Sv.pings(f).range);
-
-% Plot the interpretation mask
-hold on
-cs = cool;
-for i=1:length(layer)
-    if length(layer)>1
-        col = round(interp1(linspace(1,length(layer),size(cs,1)),1:size(cs,1),i));
-    else
-        col=1;
+for file=1:size(files.F,1)
+    snap = files.F{file,1};
+    work = files.F{file,2};
+    raw  = files.F{file,3};
+    if isempty(snap)
+        snap=work;
     end
-    patch(layer(i).x,layer(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
-end
-
-cs = hot;
-for i=1:length(school)
-    col = round(interp1(linspace(1,length(school),size(cs,1)),1:size(cs,1),i));
-    patch(school(i).x,school(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
-end
-
-% Plot erased regions
-for i=1:length(erased.channel(f).x) % loop over each ping with erased samples
-    ping = erased.channel(f).x(i);
-    ranges = erased.channel(f).y{i};
-    for j=1:size(ranges,1) % loop over each contingous block of erased samples
-        startR = ranges(j,1);
-        endR = startR + ranges(j,2);
-        patch([ping ping+1 ping+1 ping], ...
-              [startR startR endR endR]-td, 'k', ...
-            'FaceAlpha', 0.8, 'EdgeColor', 'None')
+    
+    % Read snap file
+    [school,layer,exclude,erased] = LSSSreader_readsnapfiles(snap);
+    
+    % Read raw file and convert to sv
+    [raw_header,raw_data] = readEKRaw(raw);
+    raw_cal = readEKRaw_GetCalParms(raw_header, raw_data);
+    Sv = readEKRaw_Power2Sv(raw_data,raw_cal);
+    
+    % Get the transducer depth
+    f=1;
+    if length(raw_data.pings) > 1
+        f=2; % Use 38 kHz if we can (which is usually channel 2 on IMR ships)
     end
-end
-
-% Plot exclude regions
-maxRange = max(Sv.pings(f).range);
-for i=1:length(exclude)
-    [~, startPing] = min(abs(exclude(i).startTime - Sv.pings(f).time));    
-    endPing = startPing + exclude(i).numOfPings;
-    patch([startPing startPing endPing endPing], ...
-          [0 maxRange maxRange 0]-td, 'k', 'FaceAlpha', 0.7) 
-end
-
-
+    td = double(median(raw_data.pings(f).transducerdepth));
+    
+    % Plot result
+    [fh, ih] = readEKRaw_SimpleEchogram(Sv.pings(f).Sv, 1:length(Sv.pings(f).time), Sv.pings(f).range);
+    
+    % Plot the interpretation mask
+    hold on
+    cs = cool;
+    for i=1:length(layer)
+        if length(layer)>1
+            col = round(interp1(linspace(1,length(layer),size(cs,1)),1:size(cs,1),i));
+        else
+            col=1;
+        end
+        patch(layer(i).x,layer(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
+    end
+    
+    cs = hot;
+    for i=1:length(school)
+        col = round(interp1(linspace(1,length(school),size(cs,1)),1:size(cs,1),i));
+        patch(school(i).x,school(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
+    end
+    
+    % Plot erased regions
+    if ~isempty(erased)
+        for i=1:length(erased.channel(f).x) % loop over each ping with erased samples
+            ping = erased.channel(f).x(i);
+            ranges = erased.channel(f).y{i};
+            for j=1:size(ranges,1) % loop over each contingous block of erased samples
+                startR = ranges(j,1);
+                endR = startR + ranges(j,2);
+                patch([ping ping+1 ping+1 ping], ...
+                    [startR startR endR endR]-td, 'k', ...
+                    'FaceAlpha', 0.8, 'EdgeColor', 'None')
+            end
+        end
+    end
+    
+    if ~isempty(exclude)
+        % Plot exclude regions
+        maxRange = max(Sv.pings(f).range);
+        for i=1:length(exclude)
+            [~, startPing] = min(abs(exclude(i).startTime - Sv.pings(f).time));
+            endPing = startPing + exclude(i).numOfPings;
+            patch([startPing startPing endPing endPing], ...
+                [0 maxRange maxRange 0]-td, 'k', 'FaceAlpha', 0.7)
+        end
+    end
 end
