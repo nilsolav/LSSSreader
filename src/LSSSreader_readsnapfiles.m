54af5ae2d5950f9f4155523367ac91a106b15b50
nilsolav <nils.olav.handegard@imr.no>
2017-03-21 20:13:50 +0100

    Structure problem in the excluded ping ranges Fixed the {1} vs {i} problem the non Gavin way. Ugly and works...

--- src/LSSSreader_readsnapfiles.m
+++ src/LSSSreader_readsnapfiles.m
@@ -35,9 +35,16 @@ if isfield(D.snap.regionInterpretation.exclusionRanges, 'timeRange')
     timeRange = D.snap.regionInterpretation.exclusionRanges.timeRange;
     nsE = length(timeRange);
     for i = 1:nsE
-        t = str2double(timeRange{i}.Attributes.start);
+        % Gavin: I know you don't like this. It works. You fix?
+        if nsE==1
+            t = str2double(timeRange.Attributes.start);
+            t2 = str2double(timeRange.Attributes.numberOfPings);
+        else
+            t = str2double(timeRange{i}.Attributes.start);
+            t2 = str2double(timeRange{i}.Attributes.numberOfPings);
+        end
         exclude(i).startTime = unixTimeToMatlab(t); %#ok<*AGROW>
-        exclude(i).numOfPings = str2double(timeRange{i}.Attributes.numberOfPings);
+        exclude(i).numOfPings = t2;
     end
 end
 
