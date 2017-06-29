68663fe1436b765cc3cc908724f5a6f14c49426b
nilsolav <nils.olav.handegard@imr.no>
2017-06-29 11:18:47 +0200

    Plot snap files the plotting of the snap files are now frequency dependent, i.e. the school box is only plotted in cases where there are a school box for that particular frequency.

--- src/LSSSreader_plotsnapfiles.m
+++ src/LSSSreader_plotsnapfiles.m
@@ -15,6 +15,9 @@ if nargin<6
     td=0;
 end
 
+% True if the ID should be plotted
+plID=true;
+
 if ~isempty(layer)
     cs = cool;
     for i=1:length(layer)
@@ -51,32 +54,38 @@ if ~isempty(layer)
     end
 end
 
+Idstring=[];
 % Plot the interpretation school
 if ~isempty(school)
     cs = hot;
+    % Loop over schools
     for i=1:length(school)
-        col = round(interp1(linspace(1,length(school),size(cs,1)),1:size(cs,1),i));
-        patch(school(i).x,school(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
-        
-        % Plot the Id string
-        if isempty(school(i).channel)
-            Idstring='No species ID';
-        else
-            Idstring=[];
-                for ch = 1:length(school(i).channel)
-            if isfield(school(i).channel(ch),'species')
-                    for sp=1:length(school(i).channel(ch).species)
-                        if strcmp(school(i).channel(ch).frequency,f)
+        % Plot only non empty schools (since we do not know whether an
+        % empty school is assiciated to a frequency)
+        if ~isempty(school(i).channel)
+            % Loop over channels 
+            for ch = 1:length(school(i).channel)
+                % Plot only the relevant frequency
+                if strcmp(school(i).channel(ch).frequency,f)
+                    col = round(interp1(linspace(1,length(school),size(cs,1)),1:size(cs,1),i));
+                    patch(school(i).x,school(i).y-td,cs(col,:),'FaceColor',cs(col,:),'FaceAlpha',.3)
+                    % get hte ID string for this patch and freq
+                    if isfield(school(i).channel(ch),'species')
+                        Idstring=[];
+                        for sp=1:length(school(i).channel(ch).species)
                             Idstring =[Idstring, ['ID:',school(i).channel(ch).species(sp).speciesID,' fraction:',school(i).channel(ch).species(sp).fraction,';']];
                         end
+                    else
+                        Idstring='No species ID';% No ID but has frequency so it is ok
+                    end
+                    if plID
+                        text(school(i).x(1),school(i).y(1)-td,Idstring)
                     end
                 end
             end
         end
-        text(school(i).x(1),school(i).y(1)-td,Idstring)
     end
 end
-
 % Gavin: can you fix these? I think we need to use a similar structure as
 % school and layer, if at all poassible.
 
